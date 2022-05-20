5376a73ba8349989d5a7441a2f49bb71e1ec6f9e
noam <noamkadosh91@gmail.com>
2022-05-20 01:04:20 -0700

    fixing bundle size

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,12 @@
 import commonjs from '@rollup/plugin-commonjs'
 import dts from 'rollup-plugin-dts'
 import esbuild from 'rollup-plugin-esbuild'
+import { externals } from 'rollup-plugin-node-externals'
 import json from '@rollup/plugin-json'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
-import path from 'path'
+import path from 'node:path'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import { terser } from 'rollup-plugin-terser'
 import typescript from '@rollup/plugin-typescript'
 
 const packageJson = require('./package.json')
@@ -11,8 +14,8 @@ const packageJson = require('./package.json')
 // ignore Circular Dependency warning from node_modules
 const onwarn = (warning, rollupWarn) =>
 	warning.code === 'CIRCULAR_DEPENDENCY' &&
-	warning.importer.indexOf(path.normalize('node_modules')) >= 0
-		? null
+	warning.importer.includes(path.normalize('node_modules'))
+		? undefined
 		: rollupWarn(warning)
 
 const config = [
@@ -31,11 +34,25 @@ const config = [
 			}
 		],
 		plugins: [
+			peerDepsExternal(),
+			externals(),
 			nodeResolve({ preferBuiltins: true }),
 			commonjs(),
 			json(),
 			typescript({ tsconfig: './tsconfig.json' }),
-			esbuild()
+			esbuild(),
+			terser({
+				ecma: 2020,
+				mangle: { toplevel: true },
+				compress: {
+					module: true,
+					toplevel: true,
+					unsafe_arrows: true,
+					drop_console: true,
+					drop_debugger: true
+				},
+				output: { quote_style: 1 }
+			})
 		],
 		external: ['react', 'canvas'],
 		onwarn
